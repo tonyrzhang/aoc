b76029503c484db7dce72288e3b9e35b54ed89f8
Tony Zhang <tony@optimizely.com>
2016-03-11 11:38:11 -0800

    Adding 18.1/2

---
+++ 18/main1.js
@@ -0,0 +1,115 @@
+var fs = require('fs'),
+    readline = require('readline');
+
+var rd = readline.createInterface({
+  input: fs.createReadStream('input'),
+  output: process.stdout,
+	terminal: false
+});
+
+var grid = [];
+// var emptyRow = [];
+// for (var i = 0; i < 102; i++) {
+// 	emptyRow.push({state: false, dead: true});
+// }
+// grid.push(emptyRow);
+
+function findLitNeighbors(i ,j) {
+	var sum = 0;
+	var rightSide = i === 99;
+	var leftSide = i === 0;
+	var topSide = j === 0;
+	var bottomSide = j === 99;
+
+	if (!leftSide && !topSide && grid[i - 1][j - 1]) {
+		sum++;
+	}
+
+	if (!topSide && grid[i][j - 1]) {
+		sum++;
+	}
+
+	if (!rightSide && !topSide && grid[i + 1][j - 1]) {
+		sum++;
+	}
+
+	if (!leftSide && grid[i - 1][j]) {
+		sum++;
+	}
+
+	if (!rightSide && grid[i + 1][j]) {
+		sum++;
+	}
+
+	if (!leftSide && !bottomSide && grid[i - 1][j + 1]) {
+		sum++;
+	}
+
+	if (!bottomSide && grid[i][j + 1]) {
+		sum++;
+	}
+
+	if (!rightSide && !bottomSide && grid[i + 1][j + 1]) {
+		sum++;
+	}
+
+	return sum;
+}
+
+function animate() {
+	var newGrid = [];
+	for (var i = 0; i < 100; i++) {
+		var row = [];
+		for(var j = 0; j < 100; j++) {
+			var neighborsLit = findLitNeighbors(i, j);
+			if (grid[i][j]) {
+				if(neighborsLit === 2 || neighborsLit === 3) {
+					row.push(true);
+				}
+				else {
+					row.push(false);
+				}
+			}
+			else {
+				if (neighborsLit === 3) {
+					row.push(true);
+				}
+				else {
+					row.push(false);
+				}
+			}
+		}
+		newGrid.push(row)
+	}
+	grid = newGrid;
+}
+
+rd.on('line', function(line) {
+	var row = [];
+	for (var i = 0; i < line.length; i++) {
+		var char = line[i];
+		if (char === '.') {
+			row.push(false);
+		}
+		else if (char === '#') {
+			row.push(true);
+		}
+	}
+	grid.push(row);
+});
+
+rd.on('close', function(){
+	for (var i = 0; i < 100; i++) {
+		animate();
+	}
+
+	var lights = 0;
+	for (var i = 0; i < 100; i++) {
+		for (var j = 0; j < 100; j++) {
+			if (grid[i][j]) {
+				lights++;
+			}
+		}
+	}
+	console.log(lights);
+});
